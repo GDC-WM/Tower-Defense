1fb41b6adeca5682909cadc2c099e3fa402f326d
olgiordano <olgiordano@email.wm.edu>
2019-04-04 20:45:49 -0400

    Moved fullscreen call so it wouldn't break things.

--- src/GameEngine/Engine/Engine.py
+++ src/GameEngine/Engine/Engine.py
@@ -27,10 +27,10 @@ class Engine(QWidget):
         """Class to facilitate threading.
         """
         def __init__(self, engine):
-            QtCore.QThread.__init__(self) 
+            QtCore.QThread.__init__(self)
             self.engine = engine
             self.running = True
-        
+
         def run(self):
             """The run thread.
             """
@@ -48,7 +48,7 @@ class Engine(QWidget):
                 if self.engine.active_world:
                     self.engine.active_world.runEntities()
                     self.engine.active_world.run()
-                
+
                 if(1/60 - (time.time() - start_time)) > 0:
                     time.sleep(1/60 - (time.time() - start_time))
                 self.engine.update()
@@ -61,7 +61,6 @@ class Engine(QWidget):
         self.setFixedSize(self.screen_width, self.screen_height)
         #set scale based on relation to 1080p
         self.scale = self.screen_height/1080
-        self.showFullScreen()
 
         self.active_world = None
         self.pressed_keys = set()
@@ -69,6 +68,7 @@ class Engine(QWidget):
         self.mouse_released = set()
         self.mouse_up_instant = set()
 
+        self.showFullScreen()
         self.run_thread = self.RunThread(self)
         self.run_thread.start()
 
@@ -80,33 +80,33 @@ class Engine(QWidget):
         qp.begin(self)
 
         self.active_world and self.active_world.drawScreen(qp)
-        
+
         qp.end()
 
     def keyPressEvent(self, event):
         """lazy\n
-        event -- 
+        event --
         """
         self.pressed_keys.add(event.key())
 
     def keyReleaseEvent(self, event):
         """lazy\n
-        event -- 
+        event --
         """
         self.pressed_keys.discard(event.key())
 
     def mousePressEvent(self, event):
         """lazy\n
-        event -- 
+        event --
         """
         self.mouse_keys.add(event.button())
 
     def mouseReleaseEvent(self, event):
         """lazy\n
-        event -- 
+        event --
         """
         self.mouse_up_instant.add(event.button())
-    
+
     @staticmethod
     def start(main):
         """unknown
